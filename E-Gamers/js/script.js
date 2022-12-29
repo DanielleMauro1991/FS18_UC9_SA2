3fd8cfac125dad912e73d067bc1c200f1d0e4c8f
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2022-12-29 17:07:28 -0300

    up11
    Função menu-ativo

--- E-Gamers/js/script.js
+++ E-Gamers/js/script.js
@@ -1,16 +1,28 @@
-//alert("Arquivo externoo");//
+//Comando inicial do Jquery//
 
-function mostrarmenu ($event){ //Ao clicar aciona o menu barras e aparece o Menu//
-    let menu= document.getElementById("menu")
+$(document).ready(function(){ //Função generica, não necessita de nome//
+    $("#barras").click(function(){
+        if($("#menu").hasClass("menu-ativo")){
+            $("#menu").removeClass("menu-ativo")
+        }else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+  })
+
+//Comandos js//
+// function mostrarmenu ($event){ //Ao clicar aciona o menu barras e aparece o Menu//
+//     let menu= document.getElementById("menu")
     
     //Mudar visualização do menu//
-  if (getComputedStyle(menu).display == "none"){
-    menu.style.display = "flex" 
-  }else{
 
-    menu.style.display = "none" 
-  } 
+//   if (getComputedStyle(menu).display == "none"){ //Aparecer/ Fechar menu ao clicar//
+//     menu.style.display = "flex" 
+//   }else{
 
-    event.preventDefault()
+//     menu.style.display = "none" 
+//   } 
 
-}
+//     event.preventDefault()
+
+// }
